3776ebddbc2c4b7b533226e60da626872a02952a
Ramith Hettiarachchi <ramithuh@live.com>
2019-05-12 12:08:01 +0530

    Tested 2 ToFs + Bluetooth, and it works !

--- source-code/LICRAWM 1.5.4 - Software/main.ino
+++ source-code/LICRAWM 1.5.4 - Software/main.ino
@@ -1,17 +1,35 @@
 #include "libraries/LICRAWM-io.h"
-#include "libraries/LICRAWM-functions.h"
+#include <Wire.h>
+#include <VL53L0X.h>
 
-led LED5(LED_5);
-led powerled(LED_1);
-led statusled(LED_2);
+_led LED5(LED_5);
+_led LED1(LED_1);
+_led LED3(LED_3);
+_led LED2(LED_2);
+_led LED4(LED_4);
+_switch S2(S_2);
+_switch S3(S_3);
+_switch S4(S_4);
+
+VL53L0X Sensor3;
+VL53L0X Sensor4;
+
+#include "libraries/LICRAWM-functions.h"
+#include "libraries/LICRAWM-boot.h"
 
 void setup() {
-  powerled.on();
+  boot();
+  Serial2.begin(9600);
+  Wire.begin();
+
+  boot_tof();
+
+  LED1.on(); //booting tof done!
+
 }
 
 void loop() {
-  statusled.on();
-  LED5.blink(200);
  
-}
+  print_tof_reading();
 
+}
