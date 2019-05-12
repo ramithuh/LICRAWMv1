3d34618f86ede3c3d932ec345f77dc07a8c2c219
Ramith Hettiarachchi <ramithuh@live.com>
2019-05-12 12:07:05 +0530

    tof read function

--- source-code/LICRAWM 1.5.4 - Software/libraries/LICRAWM-functions.h
+++ source-code/LICRAWM 1.5.4 - Software/libraries/LICRAWM-functions.h
@@ -1,57 +1,7 @@
-class _led{
-    private:
-        int _pin;
+/* Contains LICRAWM BOARD FUNCTIONS */
 
-    public:
-        _led(int pin){                    //Initialize LED
-            _pin=pin;
-            pinMode(_pin, OUTPUT);
-        }
 
-        void on(int miliseconds=0){
-            digitalWrite(_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
-            delay(miliseconds);
-        }
-
-        void off(int miliseconds=0){
-            digitalWrite(_pin, LOW);   // turn the LED off (LOW is the voltage level)
-            delay(miliseconds);
-        }
-
-        void blink(int miliseconds){  //blink the LED
-            digitalWrite(_pin, HIGH);
-            delay(miliseconds);
-            digitalWrite(_pin, LOW);
-            delay(miliseconds);
-        }
-        void write(bool status){
-            digitalWrite(_pin,status);
-        }
-};
-
-class _switch{
-    private:
-        int _pin;
-
-    public:
-        _switch(int pin){                    //Initialize switch
-            _pin=pin;
-            pinMode(_pin, INPUT); 
-        }
-
-        bool on(){
-            return digitalRead(_pin);
-        }
-
-        bool off(){
-            return !digitalRead(_pin);
-        }
-        bool status(){
-            return digitalRead(_pin);
-        }
-};
-
-void LED_all_off(int miliseconds=0){
+void _LED_all_off(int miliseconds=0){
     digitalWrite(LED_1, LOW);   
     digitalWrite(LED_2, LOW); 
     digitalWrite(LED_3, LOW); 
@@ -60,11 +10,44 @@ void LED_all_off(int miliseconds=0){
     delay(miliseconds);
 }
 
-void LED_all_on(int miliseconds=0){
+void _LED_all_blink(int miliseconds=0){
+    digitalWrite(LED_1, LOW);   
+    digitalWrite(LED_2, LOW); 
+    digitalWrite(LED_3, LOW); 
+    digitalWrite(LED_4, LOW); 
+    digitalWrite(LED_5, LOW); 
+    delay(miliseconds);
+    digitalWrite(LED_1, LOW);   
+    digitalWrite(LED_2, LOW); 
+    digitalWrite(LED_3, LOW); 
+    digitalWrite(LED_4, LOW); 
+    digitalWrite(LED_5, LOW); 
+}
+
+void _LED_all_on(int miliseconds=0){
     digitalWrite(LED_1, HIGH);   
     digitalWrite(LED_2, HIGH); 
     digitalWrite(LED_3, HIGH); 
     digitalWrite(LED_4, HIGH); 
     digitalWrite(LED_5, HIGH); 
     delay(miliseconds);
+}
+
+void print_tof_reading(int x=0){
+
+  
+  //Serial.print(Sensor1.readRangeContinuousMillimeters());
+  //Serial.print(",  ");
+  Serial2.print(Sensor3.readRangeContinuousMillimeters());
+  Serial2.print(",  ");
+  Serial2.print(Sensor4.readRangeContinuousMillimeters());
+  Serial2.print(",  \n");
+  //Serial.print(Sensor4.readRangeContinuousMillimeters());
+  //Serial.print(",  ");
+  //Serial.print(Sensor5.readRangeContinuousMillimeters());
+  //Serial.print(" \n  ");
+
+  
+  delay(x);
+  
 }
