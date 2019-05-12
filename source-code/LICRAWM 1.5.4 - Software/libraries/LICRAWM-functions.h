16bdea9f54310194e6428758f5748b6dc6b24f47
Ramith Hettiarachchi <ramithuh@live.com>
2019-05-12 09:16:22 +0530

    enhancements stated at #1 was added, class names were changes since "switch" is a keyword in c++

--- source-code/LICRAWM 1.5.4 - Software/libraries/LICRAWM-functions.h
+++ source-code/LICRAWM 1.5.4 - Software/libraries/LICRAWM-functions.h
@@ -1,19 +1,21 @@
-class led{
+class _led{
     private:
         int _pin;
 
     public:
-        led(int pin){                    //Initialize LED
+        _led(int pin){                    //Initialize LED
             _pin=pin;
             pinMode(_pin, OUTPUT);
         }
 
-        void on(){
+        void on(int miliseconds=0){
             digitalWrite(_pin, HIGH);   // turn the LED on (HIGH is the voltage level)
+            delay(miliseconds);
         }
 
-        void off(){
+        void off(int miliseconds=0){
             digitalWrite(_pin, LOW);   // turn the LED off (LOW is the voltage level)
+            delay(miliseconds);
         }
 
         void blink(int miliseconds){  //blink the LED
@@ -22,5 +24,47 @@ class led{
             digitalWrite(_pin, LOW);
             delay(miliseconds);
         }
+        void write(bool status){
+            digitalWrite(_pin,status);
+        }
 };
 
+class _switch{
+    private:
+        int _pin;
+
+    public:
+        _switch(int pin){                    //Initialize switch
+            _pin=pin;
+            pinMode(_pin, INPUT); 
+        }
+
+        bool on(){
+            return digitalRead(_pin);
+        }
+
+        bool off(){
+            return !digitalRead(_pin);
+        }
+        bool status(){
+            return digitalRead(_pin);
+        }
+};
+
+void LED_all_off(int miliseconds=0){
+    digitalWrite(LED_1, LOW);   
+    digitalWrite(LED_2, LOW); 
+    digitalWrite(LED_3, LOW); 
+    digitalWrite(LED_4, LOW); 
+    digitalWrite(LED_5, LOW); 
+    delay(miliseconds);
+}
+
+void LED_all_on(int miliseconds=0){
+    digitalWrite(LED_1, HIGH);   
+    digitalWrite(LED_2, HIGH); 
+    digitalWrite(LED_3, HIGH); 
+    digitalWrite(LED_4, HIGH); 
+    digitalWrite(LED_5, HIGH); 
+    delay(miliseconds);
+}
