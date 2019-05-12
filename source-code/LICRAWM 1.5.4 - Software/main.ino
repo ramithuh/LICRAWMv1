7ed360e0bce9e29051a8d32c7450157390cf47d0
Ramith Hettiarachchi <ramithuh@live.com>
2019-05-12 23:58:02 +0530

    ToF, Gyro, Bluetooth working !

--- source-code/LICRAWM 1.5.4 - Software/main.ino
+++ source-code/LICRAWM 1.5.4 - Software/main.ino
@@ -1,6 +1,7 @@
 #include "libraries/LICRAWM-io.h"
 #include <Wire.h>
 #include <VL53L0X.h>
+#include <MPU6050_tockn.h>
 
 _led LED5(LED_5);
 _led LED1(LED_1);
@@ -13,23 +14,31 @@ _switch S4(S_4);
 
 VL53L0X Sensor3;
 VL53L0X Sensor4;
+MPU6050 mpu6050(Wire);
 
 #include "libraries/LICRAWM-functions.h"
 #include "libraries/LICRAWM-boot.h"
 
 void setup() {
-  boot();
   Serial2.begin(9600);
+  boot();
   Wire.begin();
+  LED2.on();
 
+  boot_gyro();
+  LED4.on(); //booting gyro done!
   boot_tof();
-
   LED1.on(); //booting tof done!
 
 }
 
-void loop() {
+void loop(){
+
+  serial_input_check(); 
  
   print_tof_reading();
+
+  mpu6050.update(); 
+  print_gyro_reading();
 
 }
