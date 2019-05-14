16844ef29c696cfcda1656fcc86dfb4495177761
Ramith Hettiarachchi <ramithuh@live.com>
2019-05-14 18:43:43 +0530

    added debugging options (turn gyro,tof on/off) changed static string using F() macro

--- source-code/LICRAWM 1.5.4 - Software/libraries/LICRAWM-functions.h
+++ source-code/LICRAWM 1.5.4 - Software/libraries/LICRAWM-functions.h
@@ -1,5 +1,6 @@
 /* Contains LICRAWM BOARD FUNCTIONS */
 
+#define TraceFunc()   do { Serial2.print(F("In function: ")); Serial2.println(__func__); } while (0)
 
 void _LED_all_off(int miliseconds=0){
     digitalWrite(LED_1, LOW);   
@@ -33,40 +34,58 @@ void _LED_all_on(int miliseconds=0){
     delay(miliseconds);
 }
 
-void print_tof_reading(int x=0){
 
-  
+void get_tof_reading(int miliseconds=0){
+   //uint16_t r3=Sensor3.readRangeSingleMillimeters();
+   //uint16_t r4=Sensor4.readRangeSingleMillimeters();
+   uint16_t r3;
+   uint16_t r4;
+
+  if(UPDATE_TOF){
+    r3=Sensor3.readRangeContinuousMillimeters();  //this is faster than single read
+    r4=Sensor4.readRangeContinuousMillimeters();
+  }
+  if(DEBUG_TOF==true){
   //Serial.print(Sensor1.readRangeContinuousMillimeters());
   //Serial.print(",  ");
-  Serial2.print("ToF3:");
-  uint16_t r=Sensor3.readRangeContinuousMillimeters();
-  Serial2.print(r);
-  if (r==65535){ Serial2.print(" Error: ToF3 -> TIMEOUT"); }
+        Serial2.print(F("ToF3:"));
+        Serial2.print(r3);
+        if (r3==65535){ Serial2.print(F(" Error: ToF3 -> TIMEOUT")); }
 
 
-  Serial2.print(", ToF4:");
-  r=Sensor4.readRangeContinuousMillimeters();
-  Serial2.print(r);
-  if (r==65535) { Serial2.print(" Error: ToF4 -> TIMEOUT"); }
-  Serial2.print(",  \n");
-  //Serial.print(Sensor4.readRangeContinuousMillimeters());
-  //Serial.print(",  ");
-  //Serial.print(Sensor5.readRangeContinuousMillimeters());
-  //Serial.print(" \n  ");
+        Serial2.print(F(":ToF4:"));
+        Serial2.print(r4);
+        if (r4==65535) { Serial2.print(F(" Error: ToF4 -> TIMEOUT")); }
+        Serial2.print(F("\n"));
 
-  
-  delay(x);
+        //Serial.print(Sensor4.readRangeContinuousMillimeters());
+        //Serial.print(",  ");
+        //Serial.print(Sensor5.readRangeContinuousMillimeters());
+        //Serial.print(" \n  ");
+        delay(miliseconds);
+  }
   
 }
 
-void print_gyro_reading(int x=0){
+void get_gyro_reading(int miliseconds=0){
+    float x;
+    float y;
+    float z;
+    if(UPDATE_GYRO){
+        mpu6050.update(); 
+        x=mpu6050.getAngleX();
+        y=mpu6050.getAngleY();
+        z=mpu6050.getAngleZ();
+    }
 
-    Serial2.print("angleX : ");
-    Serial2.print(mpu6050.getAngleX());
-    Serial2.print(" angleY : ");
-    Serial2.print(mpu6050.getAngleY());
-    Serial2.print(" angleZ : ");
-    Serial2.println(mpu6050.getAngleZ());
+    if(DEBUG_GYRO==true){      /* displays only if needed */
+        Serial2.print(F("angleX :"));
+        Serial2.print(x);
+        Serial2.print(F(":angleY :"));
+        Serial2.print(y);
+        Serial2.print(F(":angleZ :"));
+        Serial2.println(z);
 
-    delay(x);
+        delay(miliseconds);
+    }
 }
