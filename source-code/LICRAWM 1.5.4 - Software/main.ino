56f7a82900bbdc6d632771242b4120004d7d7908
Kithmini Herath <kithminiherath9724@gmail.com>
2019-07-08 18:29:44 +0530

    comments added

--- source-code/LICRAWM 1.5.4 - Software/main.ino
+++ source-code/LICRAWM 1.5.4 - Software/main.ino
@@ -238,10 +238,9 @@ void loop(){
          
           if(coin_colour == 0){ //RED COLOR
             make_45_degree_clockwise();
-          }else if (coin_colour == 2){
+          }else if (coin_colour == 2){ //BLUE COLOR
             make_45_degree_anticlockwise();
-          }
-          else{
+          } else{//GREEN COLOR (go straight)
             move_fixed_distance(200);
           }
            flag=0;
@@ -251,6 +250,7 @@ void loop(){
         else if (flag==1 && flag_count==3){
           md.setBrakes(400,400);
           Serial2.println("Going to place the coin!");
+          
         }
         out+=":POS:";
         out+=position;
