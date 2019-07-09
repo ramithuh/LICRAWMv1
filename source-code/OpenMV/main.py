b6080645239e2494507a11b198e1add920021543
Ramith Hettiarachchi <ramithuh@live.com>
2019-07-09 14:55:36 +0530

    detect white too?

--- source-code/OpenMV/main.py
+++ source-code/OpenMV/main.py
@@ -7,8 +7,7 @@
 # tuned using http://colorizer.org ##
 import sensor, image, time
 import pyb
-from pyb import UART                     ##communication with arduino
-uart = UART(3,9600, timeout_char = 1000) ##communication with arduino
+
 #uart is
 # TX = P4
 # RX = P5
@@ -35,7 +34,11 @@ def write_to_arduino(x):
         p_7.high()
         p_8.low()
         p_9.low()
-    else:            ##Blank message #000
+    elif(x=='white'):  #101
+        p_7.high()
+        p_8.low()
+        p_9.high()
+    else:             ##Blank message #000
         p_7.high()
         p_8.high()
         p_9.high()
@@ -57,9 +60,10 @@ def LED_white(f):
 # The below thresholds track in general red/green things. You may wish to tune them...
 thresholds = [(0, 80, 15, 127, -5, 127), # generic_red_thresholds -> index is 0 so code == (1 << 0)
                (30, 100, -128, -15, -15, 127), # generic_green_thresholds -> index is 1 so code == (1 << 1)
-              (0, 85, -128, 10, -128, -25)] # generic_blue_thresholds -> index is 2 so code == (1 << 2)
-# Codes are or'ed together when "merge=True" for "find_blobs".
-
+              (0, 85, -128, 38, -128, -25),  # generic_blue_thresholds -> index is 2 so code == (1 << 2)
+              # Codes are or'ed together when "merge=True" for "find_blobs".
+               (85, 100, -10, 10, -10, 10)]
+#(0, 30, -10, 10, -10, 10)]
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.CIF)
@@ -79,6 +83,14 @@ areas=[0,0,0] #r.g.b
 #1 green
 #2 blue
 
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.CIF)
+
+sensor.skip_frames(time = 500)
+sensor.set_auto_gain(False) # must be turned off for color tracking
+sensor.set_auto_whitebal(False) # must be turned off for color tracking
+clock = time.clock()
 
 
 LED_white(1);
@@ -95,7 +107,8 @@ LED_white(0);
 
 i=0;
 while(1):
-    print(i)
+    #print(i)
+    '''
     if(i%15==0):
         sensor.reset()
         sensor.set_pixformat(sensor.RGB565)
@@ -105,9 +118,10 @@ while(1):
         sensor.set_auto_gain(False) # must be turned off for color tracking
         sensor.set_auto_whitebal(False) # must be turned off for color tracking
         clock = time.clock()
+    '''
     i+=1;
 
-    #LED_white(1);
+    LED_white(0);
     #print("running", str(50-i), areas)
 
     clock.tick()
@@ -126,7 +140,7 @@ while(1):
             areas[0]+=blob.area()
             img.draw_rectangle(blob.rect())
             img.draw_cross(blob.cx(), blob.cy())
-            img.draw_string(blob.x() + 2, blob.y() + 2, "red")
+            img.draw_string(blob.x() + 2, blob.y() + 2, "red"+str(blob.area()))
             #uart.write("red\n")  ##communication with arduino
             write_to_arduino("red")
 
@@ -134,7 +148,7 @@ while(1):
             areas[1]+=blob.area()
             img.draw_rectangle(blob.rect())
             img.draw_cross(blob.cx(), blob.cy())
-            img.draw_string(blob.x() + 2, blob.y() + 2, "green")
+            img.draw_string(blob.x() + 2, blob.y() + 2, "green"+str(blob.area()))
             #uart.write("green\n") ##communication with arduino
             write_to_arduino("green")
 
@@ -142,9 +156,16 @@ while(1):
             areas[2]+=blob.area()
             img.draw_rectangle(blob.rect())
             img.draw_cross(blob.cx(), blob.cy())
-            img.draw_string(blob.x() + 2, blob.y() + 2, "blue")
+            img.draw_string(blob.x() + 2, blob.y() + 2, "blue"+str(blob.area()))
             #uart.write("blue\n")  ##communication with arduino
             write_to_arduino("blue")
+
+        elif (blob.code() == 8 and blob.area()>20000) : # b code
+            LED_white(1);
+            img.draw_rectangle(blob.rect())
+            img.draw_cross(blob.cx(), blob.cy())
+            img.draw_string(blob.x() + 2, blob.y() + 2, "White"+str(blob.area()))
+            write_to_arduino("white")
         else:
             write_to_arduino("nothing")
 
